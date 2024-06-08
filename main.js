2f0515f481b5add5982094a29a0d932c6584061e
Arthur Vinicios <128225877+Mistzin07@users.noreply.github.com>
2024-06-08 12:07:56 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,60 @@
-window.addEventListener('load', () => {
 
+window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition((position) => {
         const latitude = position.coords.latitude
         const longitude = position.coords.longitude
-        const getWheather = async () => {
+
+        const getWeather = async () => {
             const getData = await fetch(`http://api.weatherapi.com/v1/current.json?key=069d1a9000c54416ac8121423240106&q=${latitude},${longitude}&lang=pt`)
+
+
             const res = await getData.json()
-
-            console.log(res.current.condition.text)//condição
-            console.log(res.location.name) //cidade
-            console.log(res.current.temp_c)//temperadtura
-            console.log(res.current.feelslike_c)//sensação termica
-            console.log(res.current.condition.wind_kph)//velcidade do vento
-
-            const condicao = document.createElement('p')
-            condicao.innerHTML=res.current.condition.text
             
-            document.querySelector('.dados').appendChild(condicao)
+            const condicaoContainer = document.querySelector('.condicao')
+            const localAtualContainer = document.querySelector('.local-atual')
+
+            console.log(res.current.condition.text) //Condição
+
+            console.log(res.location.name) //Cidade
+            console.log(res.current.temp_c) //Temperatura
+            console.log(res.current.feelslike_c) //Sensação térmica
+            console.log(res.current.condition.wind_kph) //Velocidade do vento                   
+
+            
+            const icone = document.createElement('img')
+            const condicao = document.createElement('p')
+
+            const cidade = document.createElement('p')
+            const Temperatura = document.createElement('p')
+            const sensacaoTermica = document.createElement('p')
+            const velocidadeDoVento = document.createElement('p')
+            
+            condicao.innerHTML = res.current.condition.text
+            icone.setAttribute('src','https:'+res.current.condition.icon)
+
+            
+            cidade.innerHTML = res.location.name
+            Temperatura.innerHTML = res.current.temp_c + '°C'
+            sensacaoTermica.innerHTML =  res.current.feelslike_c + ''
+            velocidadeDoVento.innerHTML = res.current.wind_kph + 'Kh'
+            
+            
+
+            condicaoContainer.appendChild(icone)
+            condicaoContainer.appendChild(Temperatura)
+            condicaoContainer.appendChild(condicao)
+
+            
+            localAtualContainer.appendChild(sensacaoTermica)
+            localAtualContainer.appendChild(cidade)
+            localAtualContainer.appendChild(velocidadeDoVento)
+            
+
+
         }
 
-
-        getWheather()
+        getWeather()
     })
-
 })
 
 
